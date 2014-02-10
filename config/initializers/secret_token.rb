909ad0651b997191a1985a61674365befe0e2cc5
Christian Seidel <sh3xbeer@gmail.com>
2014-02-10 22:59:55 +0100

    skelleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorBookmarks::Application.config.secret_key_base = '60a4306f41a5710d680fc5f28aa8ff32483a76213a5d292a60211c3da0479a9d4c40be819dec84deb67709135ade96df9f6726615e0eb5d2402a6842bea6e6dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorBookmarks::Application.config.session_store :cookie_store, key: '_ror-bookmarks_session'
